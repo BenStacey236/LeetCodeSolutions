64fe689a8f59dea0d70a2d1aa06f81b6824999f1
BenStacey236 <dealt-runner0c@icloud.com>
2024-10-17 14:17:01 +0100

    isAnagram First Solution

---
+++ isAnagram.py
@@ -0,0 +1,15 @@
+def isAnagram(s: str, t: str) -> bool:
+    sSet = set(s)
+
+    if not sSet == set(t):
+        return False
+
+    for letter in sSet:
+        if not s.count(letter) == t.count(letter):
+            return False
+
+    return True
+    
+
+if __name__ == '__main__':
+    print(isAnagram('aacc', 'ccca'))
