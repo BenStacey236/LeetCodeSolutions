7ccf3cd08d6dc25fda7b3ebb0a7db24e181871c1
BenStacey236 <dealt-runner0c@icloud.com>
2024-10-18 11:12:36 +0100

    Slight higher memory efficiency

--- topKFrequent.py
+++ topKFrequent.py
@@ -1,23 +1,23 @@
 # Medium Problem
 
 def topKFrequent(nums: list[int], k: int) -> list[int]:
-    numberFrequencies = {number:nums.count(number) for number in set(nums)}
+    numSet = set(nums)
 
-    maxes = []
-    for _ in range(0, k):
-        maxValue = 0
-        for key, value in numberFrequencies.items():
-            if value > maxValue:
-                maxValue = value
-                maxKey = key
-            
-        maxes.append(maxKey)
-        numberFrequencies.pop(maxKey)
+    while len(numSet) != k:
+        toRemove = []
+        for item in numSet:
+            if item not in nums:
+                toRemove.append(item)
+            else:
+                nums.remove(item)
 
-    return maxes
+        for num in toRemove:
+            numSet.remove(num)
 
+    return list(numSet)
 
+    
 if __name__ == "__main__":
-    print(topKFrequent([1, 2], 2))
+    print(topKFrequent([5,3,1,1,1,3,73,1], 2))
     
     
