7f865e333e541bead027c3d8c7ec57b129290ffe
BenStacey236 <dealt-runner0c@icloud.com>
2024-10-19 12:19:36 +0100

    groupAnagrams First Solution

---
+++ groupAnagrams.py
@@ -0,0 +1,18 @@
+# Medium Problem
+
+def groupAnagrams(strs: list[str]) -> list[list[str]]:
+    anagrams = {}
+    
+    for string in strs:
+        sortedStr = ''.join(sorted(string))
+        if sortedStr in anagrams:
+            anagrams[sortedStr].append(string)
+        else:
+            anagrams[sortedStr] = [string]
+
+    return [value for value in anagrams.values()]
+
+
+if __name__ == "__main__":
+    print(groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
+        
