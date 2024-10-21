c7c218b80cf51613b915e5f1235d1c366e4be452
BenStacey236 <dealt-runner0c@icloud.com>
2024-10-21 11:42:52 +0100

    Implimented two pointers method

--- Python/Easy/isPalindrome.py
+++ Python/Easy/isPalindrome.py
@@ -3,7 +3,17 @@
 def isPalindrome(s: str) -> bool:
     s = [letter.lower() for letter in s if letter.isalnum()]
 
-    return s == list(reversed(s))
+    frontP = 0
+    backP = len(s) - 1
+
+    while frontP < backP:
+        if s[frontP] != s[backP]:
+            return False
+        else:
+            frontP += 1
+            backP -=1
+
+    return True
 
 
 input = "A man, a plan, a canal: Panama"
