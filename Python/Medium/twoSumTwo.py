f1863131a344c36a2bd4dc3838e35c101604ba80
BenStacey236 <dealt-runner0c@icloud.com>
2024-10-21 12:26:49 +0100

    twoSumTwo First Solution

---
+++ Python/Medium/twoSumTwo.py
@@ -0,0 +1,21 @@
+# Medium Problem
+
+def twoSumTwo(numbers: list[int], target: int) -> list[int]:
+    frontP, backP = 0, len(numbers) - 1
+
+    while frontP < backP:
+        if numbers[frontP] + numbers[backP] == target:
+            return [frontP+1, backP+1]
+        
+        elif numbers[frontP] + numbers[backP] < target:
+            frontP += 1
+
+        else:
+            backP -= 1
+
+
+input = [2,7,11,15]
+target = 9
+
+if __name__ == "__main__":
+    print(twoSumTwo(input, target))
