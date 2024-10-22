d9cfee128c758406e83769de8af4d890375da907
BenStacey236 <dealt-runner0c@icloud.com>
2024-10-22 11:52:43 +0100

    maxArea First Solution

---
+++ Python/Medium/maxArea.py
@@ -0,0 +1,24 @@
+# Medium Problem
+
+def maxArea(height: list[int]) -> int:
+    frontP, backP = 0, len(height) - 1
+    maximumArea = 0
+
+    while frontP < backP:
+        area = min(height[frontP], height[backP]) * (backP - frontP)
+        maximumArea = max(area, maximumArea)
+
+        if height[frontP] < height[backP]:
+            frontP += 1
+        elif height[frontP] > height[backP]:
+            backP -= 1
+        else:
+            frontP += 1
+
+    return maximumArea
+
+
+input = [1,3,2,5,25,24,5]
+
+if __name__ == "__main__":
+    print(maxArea(input))
