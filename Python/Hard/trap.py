aef304a8b2993f9da7a2ea05238c32d7afdac56d
BenStacey236 <dealt-runner0c@icloud.com>
2024-10-23 12:22:03 +0100

    More memory efficient solution

--- Python/Hard/trap.py
+++ Python/Hard/trap.py
@@ -1,24 +1,26 @@
 # Hard Problem
 # Problem 42
 
-def trap(heights: list[int]) -> int:
+def trapMemory(heights: list[int]) -> int:
     trapped = 0
-    maxLefts = {}
-    maxRights = {}
-    
-    maxL = 0
-    maxR = 0
-    for i, height in enumerate(heights):
-        r = len(heights) - i - 1
-        maxR = max(maxR, heights[r])
-        maxL = max(maxL, height)
-        
-        maxLefts[i] = maxL
-        maxRights[r] = maxR
+    maxL, maxR = 0, 0
 
-    for i, height in enumerate(heights):
-        toTrap = min(maxLefts[i], maxRights[i]) - height
-        if toTrap > 0: trapped += toTrap
+    frontP, backP = 0, len(heights) - 1
+    while frontP < backP:
+        maxL = max(maxL, heights[frontP])
+        maxR = max(maxR, heights[backP])
+
+        if heights[frontP] < heights[backP]:
+            frontP += 1
+            toTrap =  maxL - heights[frontP]
+            if toTrap > 0:
+                trapped += toTrap
+
+        else:
+            backP -= 1
+            toTrap = maxR - heights[backP]
+            if toTrap > 0:
+                trapped += toTrap
 
     return trapped
 
@@ -26,4 +28,4 @@ def trap(heights: list[int]) -> int:
 input = [0,1,0,2,1,0,1,3,2,1,2,1]
 
 if __name__ == "__main__":
-    print(trap(input))
+    print(trapMemory(input))
