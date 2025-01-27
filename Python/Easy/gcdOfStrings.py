309cb17eef410f8ae22a22bf5af1789f1a1b0ea4
BenStacey236 <dealt-runner0c@icloud.com>
2025-01-27 13:11:52 +0000

    Greatest Common Divisor of Strings first solution

---
+++ Python/Easy/gcdOfStrings.py
@@ -0,0 +1,13 @@
+def gcdOfStrings(str1: str, str2: str) -> str:
+    long = str1 if len(str1) > len(str2) else str2
+    short = str1 if long == str2 else str2
+
+    for i in range(len(short), 0, -1):
+        divisor = short[:i]
+        if (divisor in long) and (divisor * (len(long) // len(divisor)) == long) and (divisor * (len(short) // len(divisor)) == short):
+            return divisor
+
+    return ""
+
+
+print(gcdOfStrings("TAUXXTAUXXTAUXXTAUXXTAUXX", "TAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXXTAUXX"))
