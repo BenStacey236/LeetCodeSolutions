cf2a85be1ff6ae3975c72cdcd08ac819ad47427d
BenStacey236 <dealt-runner0c@icloud.com>
2025-01-27 09:19:37 +0000

    Palindrome Number first attempt

---
+++ Python/Easy/palindromeNumber.py
@@ -0,0 +1,5 @@
+# Easy Problem
+# Problem 9
+
+def isPalindrome(x: int) -> bool:
+    return str(x) == str(x)[::-1]
--- Python/Medium/minRotatedArray.py
+++ Python/Medium/minRotatedArray.py
@@ -2,14 +2,19 @@
 # Problem 153
 
 def findMin(nums: list[int]) -> int:
-    for i in range(len(nums) - 2):
-        if nums[i] > nums[i+1]:
-            return nums[i+1]
-        
-    return nums[0]
+    frontP, backP = 0, len(nums) - 1
+
+    while frontP < backP:
+        mid = (frontP + backP) // 2
+        if nums[mid] < nums[frontP]:
+            backP = mid - 1
+        else:
+            frontP = mid + 1
+
+    return nums[frontP]
 
 
-input = [5, 6, 7, 0, 1, 2, 3, 4]
+input = [7, 8, 9, 10, 0, 1, 2, 3, 4, 5, 6]
 
 if __name__ == "__main__":
     print(findMin(input))
