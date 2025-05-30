7502f9ae597208cfcfb89f057b947c6e602f53fb
psybs10 <psybs10@exmail.nottingham.ac.uk>
2025-05-30 14:44:29 +0100

    Remove stars more efficient solution

--- Python/Medium/removeStars.py
+++ Python/Medium/removeStars.py
@@ -1,19 +1,6 @@
 # Medium Problem
 # Problem 2390
 
-'''def removeStars(s: str) -> str:
-    i = 0
-
-    while 0 <= i < len(s):
-        if s[i] == "*":
-            s = s[:i-1] + s[i+1:]
-            i = i - 1
-        else:
-            i += 1
-
-    return s'''
-
-
 def removeStars(s: str) -> str:
     output = []
 
