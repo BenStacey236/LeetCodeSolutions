fbff4cd23f66edbf3107240eb3457a7f288c97ad
psybs10 <psybs10@exmail.nottingham.ac.uk>
2026-02-09 00:40:59 +0000

    [Problem 52] More efficient solution for N Queens

--- Python/Hard/52_totalNQueens.py
+++ Python/Hard/52_totalNQueens.py
@@ -4,44 +4,30 @@
 from typing import List
 
 
-def print_board(board: List[List[int]]) -> None:
-    for row in board:
-        for cell in row:
-            print(cell, end="")
-        print()
-
-    print()
-
-
 def totalNQueens(n: int) -> int:
 
-    def place_piece(board: List[List[int]], x: int, y: int) -> List[List[int]]:
-        newBoard = []
-        for r in range(n):
-            newRow = []
-            for c in range(n):
-                if r == y or c == x or abs(r-y) == abs(c-x):
-                    newRow.append(1)
-                else:
-                    newRow.append(board[r][c])
-            newBoard.append(newRow)
-
-        return newBoard
-
-    def explore(board: List[List[int]], remaining: int, count: int) -> int:
+    def explore(remaining: int, count: int, locations: List[tuple[int, int]]) -> int:
 
         if remaining == 0:
             return count + 1
 
         y = n-remaining
-        for x, cell in enumerate(board[y]):
-            if cell == 1: continue
+        for x in range(n):
 
-            count = explore(place_piece(board, x, y), remaining-1, count)
+            collision = False
+            for lx, ly in locations:
+                if ly == y or lx == x or abs(ly-y) == abs(lx-x):
+                    collision = True
+                    break
+
+            if collision:
+                continue
+            
+            count = explore(remaining-1, count, locations + [(x,y)])
 
         return count
 
-    return explore([[0 for _ in range(n)] for _ in range(n)], n, 0)
+    return explore(n, 0, [])
 
 if __name__ == "__main__":
     
